0ddea5e870636d44994a6993844df4f3f57301e9
khanm162 <81820489+khanm162@users.noreply.github.com>
2026-02-25 11:40:21 -0500

    Fix module system and clean lightspeed service

--- api/test-ls-products.ts
+++ api/test-ls-products.ts
@@ -1,4 +1,4 @@
-const type { VercelRequest, VercelResponse } from "@vercel/node";
+import type { VercelRequest, VercelResponse } from "@vercel/node";
 const { getItems } from "../lib/lightspeed";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
